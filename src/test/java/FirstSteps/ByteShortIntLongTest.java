0aeef8d42d90a1c00832a2fc7563b040677b6d92
Caio Antonio <caiocichetti08@gmail.com>
2021-08-31 19:15:13 -0300

    chore: updated test

--- src/test/java/FirstSteps/ByteShortIntLongTest.java
+++ src/test/java/FirstSteps/ByteShortIntLongTest.java
@@ -3,14 +3,13 @@ package FirstSteps;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Types Primitives Section")
-public class ByteShortIntLong {
+public class ByteShortIntLongTest {
     @Test
     @DisplayName("must return the minimum value of each primitive type\n")
     void testMinimumOfPrimitiveType() {
-        assertEquals(-2147483648, Integer.MIN_VALUE);
+        assertEquals(Integer.MIN_VALUE, -2147483648);
     }
 }
