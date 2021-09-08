0917601fc70371e71297025e6a77c76d0f435aab
Caio Antonio <caiocichetti08@gmail.com>
2021-09-08 18:39:13 -0300

    Operators

--- src/main/java/FirstSteps/Operators.java
+++ src/main/java/FirstSteps/Operators.java
@@ -3,6 +3,18 @@ package FirstSteps;
 public class Operators {
     public static void main(String[] args) {
         int sum = 1 + 2;
+        System.out.println("1 + 2 = " + sum);
 
+        int subtract = sum - 1;
+        System.out.println("3 - 1 = " + subtract);
+
+        int multiplication = subtract * 10;
+        System.out.println("2 * 10 = " + multiplication);
+
+        int divide = multiplication / 5;
+        System.out.println("20 / 10 = " + divide);
+
+        int modulo = divide % 3;
+        System.out.println("4 % 3 = " + modulo);
     }
 }
